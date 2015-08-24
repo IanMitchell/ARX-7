b02f45dd2548fc08d1c34af4d802a90e93a7ee05
Ian Mitchell <ianm@zillow.com>
2015-08-24 14:59:08 -0700

    ARX-7 Test Cases
    Not quite sure how to test ARX-7 yet, but these are the test cases I
    want to run eventually

--- test/helpers.js
+++ test/helpers.js
@@ -14,3 +14,20 @@ export class Client {
     this.lastMessage = null;
   }
 }
+
+export class Command {
+  constructor(client) {
+    this.client = client;
+  }
+
+  message(from, to, text, message) {
+    this.client.say(to, text);
+  }
+}
+
+export class Choose extends Command {}
+export class Imgur extends Command {}
+export class Order extends Command {}
+export class Reply extends Command {}
+export class Twitter extends Command {}
+export class Youtube extends Command {}
---
+++ test/test_arx7.js
@@ -0,0 +1,40 @@
+import assert from "assert";
+import "./helpers.js";
+
+describe('ARX-7', () => {
+  describe('Identifies with NickServ', () => {
+    it('should identify on connect', () => {
+      assert(false, 'Not implemented');
+    });
+  });
+
+  describe('Responds to CTCP Version', () => {
+    it('should respond with VERSION', () => {
+      assert(false, 'Not implemented');
+    });
+  });
+
+  describe('Responds to Bans', () => {
+    it('should attempt to rejoin', () => {
+      assert(false, 'Not implemented');
+    });
+  });
+
+  describe('Handles Messages', () => {
+    it('should respond to PM', () => {
+      assert(false, 'Not implemented');
+    });
+
+    it('should handle uppercase channel names', () => {
+      assert(false, 'Not implemented');
+    });
+
+    it('should send message to plugins', () => {
+      assert(false, 'Not implemented');
+    });
+
+    it('should channel-restrict plugins', () => {
+      assert(false, 'Not implemented');
+    });
+  });
+});
