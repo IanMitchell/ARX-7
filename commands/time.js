379302868a5e6eae05e9e0502d588958db8675f8
Raphael Ho <boo.rho@gmail.com>
2015-08-30 00:31:00 -0700

    Refactored using promises and map instead of hash

--- commands/time.js
+++ commands/time.js
@@ -3,35 +3,40 @@
  */
 
 import {Command} from './command.js';
-var tz = require('timezone/loaded');
+import timezone from 'timezone/loaded';
 
 export class Time extends Command {
   message(from, to, text, message) {
     let timezoneMatch = text.match(/^[.!]time (.*)/);
-    if(timezoneMatch) {
-      let timezoneQuery = this.abbreviationMap(timezoneMatch[1]);
-      this.getTime(timezoneQuery, (result) => {
-        this.send(to, `${from}: ${result}`);
-      });
-    }
+    return new Promise((resolve, reject) => {
+      if(timezoneMatch) {
+        let timezoneQuery = this.abbreviationMap(timezoneMatch[1]);
+        this.getTime(timezoneQuery).then((result) => {
+          this.send(to, `${from}: ${result}`);
+        });
+      }
+      resolve();
+    });
   }
 
-  getTime(timezone, callback) {
-    let formattedTime = tz(new Date(), "%c", timezone);
-    callback(formattedTime);
+  getTime(timezoneQuery) {
+    return new Promise((resolve, reject) => {
+      resolve(timezone(new Date(), "%c", timezoneQuery));
+    })
   }
 
   // Timezone abbreviations are ambiguous but I'm smarter than timezones
   abbreviationMap(abbr) {
-    let abbreviationToTimezone = {
-      'JST': 'Asia/Tokyo',
-      'EST': 'America/New_York',
-      'EDT': 'America/New_York',
-      'CST': 'America/Chicago',
-      'CDT': 'America/Chicago',
-      'PST': 'America/Los_Angeles',
-      'PDT': 'America/Los_Angeles'
-    };
-    return abbreviationToTimezone[abbr] || abbr;
+    let abbrArray = [
+      ['JST', 'Asia/Tokyo'],
+      ['EST', 'America/New_York'],
+      ['EDT', 'America/New_York'],
+      ['CST', 'America/Chicago'],
+      ['CDT', 'America/Chicago'],
+      ['PST', 'America/Los_Angeles'],
+      ['PDT', 'America/Los_Angeles']
+    ];
+    let abbrMap = new Map(abbrArray);
+    return abbrMap.get(abbr) || abbr;
   }
 }
