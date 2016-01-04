315e1102d3e8f99f0b70fd7199f96afd44d20dca
Ian Mitchell <ianm@zillow.com>
2016-01-04 15:36:09 -0800

    Moved Reply Map to Constructor

--- src/commands/reply.js
+++ src/commands/reply.js
@@ -4,18 +4,21 @@ import { Command } from './command.js';
 const log = debug('Reply');
 
 export class Reply extends Command {
+  constructor(client) {
+    super(client);
+    this.responses = new Map();
+    this.responses.set('ping', 'pong');
+    this.responses.set('bot respond', "I'm a pretty stupid bot.");
+    this.responses.set('bot be nice', 'sorry :(');
+    this.responses.set('gj bot', 'thx');
+    this.responses.set('thx bot', 'np');
+    this.responses.set('bot pls', '( ¬‿¬)');
+    this.responses.set('!bugreport', 'Hi! To file a feature request or bug, go to: https://github.com/IanMitchell/ARX-7');
+  }
+
   message(from, to, text) {
     return new Promise(resolve => {
-      const responses = new Map();
-      responses.set('ping', 'pong');
-      responses.set('bot respond', "I'm a pretty stupid bot.");
-      responses.set('bot be nice', 'sorry :(');
-      responses.set('gj bot', 'thx');
-      responses.set('thx bot', 'np');
-      responses.set('bot pls', '( ¬‿¬)');
-      responses.set('!bugreport', 'Hi! To file a feature request or bug, go to: https://github.com/IanMitchell/ARX-7');
-
-      const reply = responses.get(text.toLowerCase());
+      const reply = this.responses.get(text.toLowerCase());
       if (reply) {
         log(`${from} ${reply} reply`);
         this.send(to, reply);
