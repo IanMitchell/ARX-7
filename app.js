8c2d24ad950fb5788c0b42d2090665a4c5dc33d0
Ian Mitchell <ianm@zillowgroup.com>
2017-04-12 22:46:16 -0700

    Dotenv Load Fix

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const debug = require('debug');
 const log = debug('Boot');
 
+require('dotenv').config();
+
 if (process.env.NODE_ENV === 'production') {
   log('Booting Production Build');
   require('./lib/bot.js');
 } else {
   log('Booting Development Build');
   log('Reading from `.env`');
-  require('dotenv').config();
   require('./src/bot.js');
 }
